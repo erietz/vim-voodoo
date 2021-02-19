7bb1458650bded461f220490d6b8006cc2367bbd
Ethan Rietz <ewrietz@gmail.com>
2021-02-18 20:44:25 -0600

    airline themes work for lowercase themes without numbers........

--- python/colors.py
+++ python/colors.py
@@ -11,16 +11,25 @@ OUT_DIR = ROOT_DIR / 'lua'
 with open('python/all_styles.json', 'r') as f:
     styles = json.load(f)
 
-def fill_template(template, hex_list):
+def fill_template(template, hex_list, style):
     for i in range(7):
         template = template.replace(f'placeholder_{i}', f'"{hex_list[i]}"')
+        template = template.replace('placeholder_theme_name', f'{style}')
     return template
 
-for style in styles:
-    with open(TEMPLATE_FILE, 'r') as infile, open(OUT_DIR/f'{style}.lua', 'w') as outfile:
-        template = infile.read()
-        hex_list = styles[style]['hex']
-        contents = fill_template(template, hex_list)
-        outfile.write(contents)
+def generate_all_files(template_file, out_dir, extension):
+    for style in styles:
+        with open(template_file, 'r') as infile, open(out_dir/f'{style}.{extension}', 'w') as outfile:
+            template = infile.read()
+            hex_list = styles[style]['hex']
+            contents = fill_template(template, hex_list, style)
+            outfile.write(contents)
 
-print('all converted')
+generate_all_files(TEMPLATE_FILE, OUT_DIR, 'lua')
+print('all colorschemes converted')
+
+AIRLINE_DIR = ROOT_DIR / 'autoload/airline/themes/'
+AIRLINE_TEMPLATE = ROOT_DIR / 'autoload/airline/template.vim'
+
+generate_all_files(AIRLINE_TEMPLATE, AIRLINE_DIR, 'vim')
+print('all airline themes converted')
